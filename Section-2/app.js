d6436d5633993a3e5cd0fbe4a1a54d8f6a247c52
Hyejin Lee <hichjay@gmail.com>
2021-08-10 23:12:25 +0900

    Section 2 finish - TypeScript Basics & Basic Types

--- Section-2/app.js
+++ Section-2/app.js
@@ -1,12 +1,11 @@
-function combine(input1, input2) {
-    var result;
-    if (typeof input1 === "number" && typeof input2 === "number")
-        result = input1 + input2;
-    else
-        result = input1.toString() + input2.toString();
-    return result;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-var combinedAges = combine(30, 26);
-console.log(combinedAges);
-var combinedNames = combine("Max", "Anna");
-console.log(combinedNames);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError("An error occurred!", 500);
--- Section-2/app.ts
+++ Section-2/app.ts
@@ -1,20 +1,15 @@
-function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: string
-) {
-  let result;
-  if (typeof input1 === "number" && typeof input2 === "number")
-    result = input1 + input2;
-  else result = input1.toString() + input2.toString();
-  return result;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+
+if (typeof userInput === "string") {
+  userName = userInput;
 }
 
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
 
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
-
-const combinedNames = combine("Max", "Anna", "as-text");
-console.log(combinedNames);
+generateError("An error occurred!", 500);
