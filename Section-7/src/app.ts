24f8a8842e9d6b558659a919efb1369587eca94d
Hyejin Lee <hichjay@gmail.com>
2022-02-20 00:30:01 +0900

    Section 7: Generics
    for flexible & reusable code
    -Generic types
    - custom generic function
    - type constraints
    - keyof constraint
    - generic class
    - generic utility types: Partial, Readonly

---
+++ Section-7/src/app.ts
@@ -0,0 +1,88 @@
+const names: Array<string> = ["Meredith", "Andrew"];
+// names[0].split('');
+
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("This is done!");
+//   }, 2000);
+// });
+
+// promise.then((data) => {
+//   data.split("");
+// });
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Meredith", hobbies: ["sports"] }, { age: 26 });
+console.log(mergedObj);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) descriptionText = "Got 1 element.";
+  else if (element.length > 1)
+    descriptionText = `Got ${element.length} elements.`;
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hi there!"));
+console.log(countAndDescribe(["Sprots", "Cooking"]));
+console.log(countAndDescribe(""));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value: " + obj[key];
+}
+
+console.log(extractAndConvert({ name: "Meredith" }, "name"));
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) !== -1)
+      this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Meredith");
+textStorage.addItem("Christine");
+textStorage.removeItem("Meredith");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+// objStorage.addItem({ name: "Max" });
+// objStorage.addItem({ name: "Manuel" });
+// // ...
+// objStorage.removeItem({ name: "Max" });
+// console.log(objStorage.getItems());
+
+interface CourseGal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {}
